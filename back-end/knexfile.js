874c817c67c6665e24161beaef6104fc4e4503df
Canaan DePriest <canaanlee01@gmail.com>
2022-08-06 16:52:40 -0600

    updates

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -20,10 +20,10 @@
      pool: { min: 1, max: 5 },
      connection: DATABASE_URL_DEVELOPMENT,
      migrations: {
-       directory: path.join(__dirname, "back-end", "src", "db", "migrations"),
+       directory: path.join(__dirname,  "src", "db", "migrations"),
      },
      seeds: {
-       directory: path.join(__dirname,"back-end", "src", "db", "seeds"),
+       directory: path.join(__dirname, "src", "db", "seeds"),
      },
      debug: !!DEBUG,
    },
@@ -32,10 +32,10 @@
      pool: { min: 1, max: 5 },
      connection: DATABASE_URL_TEST,
      migrations: {
-       directory: path.join(__dirname,"back-end", "src", "db", "migrations"),
+       directory: path.join(__dirname, "src", "db", "migrations"),
      },
      seeds: {
-       directory: path.join(__dirname, "back-end", "src", "db", "seeds"),
+       directory: path.join(__dirname,  "src", "db", "seeds"),
      },
      debug: !!DEBUG,
    },
@@ -44,10 +44,10 @@
      pool: { min: 1, max: 5 },
      connection: DATABASE_URL_PREVIEW,
      migrations: {
-       directory: path.join(__dirname, "back-end", "src", "db", "migrations"),
+       directory: path.join(__dirname, "src", "db", "migrations"),
      },
      seeds: {
-       directory: path.join(__dirname, "back-end", "src", "db", "seeds"),
+       directory: path.join(__dirname, "src", "db", "seeds"),
      },
      debug: !!DEBUG,
    },
@@ -56,10 +56,10 @@
      pool: { min: 1, max: 5 },
      connection: DATABASE_URL,
      migrations: {
-       directory: path.join(__dirname,"back-end", "src", "db", "migrations"),
+       directory: path.join(__dirname, "src", "db", "migrations"),
      },
      seeds: {
-       directory: path.join(__dirname,"back-end", "src", "db", "seeds"),
+       directory: path.join(__dirname, "src", "db", "seeds"),
      },
      debug: !!DEBUG,
    },
